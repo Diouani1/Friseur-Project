e8d6798e76377dd39d2c7f38428f84bbdcdacd8e
Diouani <diouanijalouka@gmail.com>
2023-05-29 15:10:20 +0200

    getting picture for author from G.cloud

--- controllers/post/authorPicture.js
+++ controllers/post/authorPicture.js
@@ -1,13 +1,23 @@
 import User from "../../models/User.js";
 import creatErr from "http-errors";
-import path from "path";
+import { Storage } from "@google-cloud/storage";
+
+const storage = new Storage({
+  projectId: "friseur-jalouka",
+  keyFilename: "./google_credentials.json",
+});
+
+const bucketName = "profile-jalouka-bucket";
+const bucket = storage.bucket(bucketName);
 
 const authorPicture = async (req, res, next) => {
   try {
     const user = await User.findOne({ userName: req.params.username });
 
-    const authorPicturePath = path.resolve("./", user.imgProfile.path);
-    res.sendFile(authorPicturePath);
+    const file = bucket.file(user.imgProfile.filename);
+    const fileStream = file.createReadStream();
+
+    fileStream.pipe(res);
   } catch (error) {
     next(creatErr(404, error));
   }
